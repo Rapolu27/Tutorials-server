fd6c362eabb9f3bcd25f131480214ddd41335598
Rapolu27 <153436717+Rapolu27@users.noreply.github.com>
2024-04-18 06:30:35 +0100

    Tutorials server

---
+++ src/test/java/ie/nic/student/priyanka/SpringBootDataJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package ie.nic.student.priyanka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootDataJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
